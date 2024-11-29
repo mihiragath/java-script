eed0e84b386c096929bb2822f70cb250a9fb9906
mihiragath <mihiragath8@gmail.com>
2024-11-30 02:51:13 +0530

    new

---
+++ java script basics/variables.js
@@ -0,0 +1,21 @@
+const customerId = 12345;
+let accountEmail = "mihir@gmail.com";
+var accoutPassword = "33421";
+accountCity = "porbandar";
+let accountMoney
+
+accountEmail = "m@gmail.com"
+accountPasseord = "4321"
+accountCity = "rajkot"
+
+console.log(customerId);
+console.log(accountCity);
+
+console.table([accountEmail, accoutPassword, accountCity, accountMoney]);
+
+/*
+    prefer not to use var
+        bcz in var variable there are replce all value which will not necessary to replace when only one value are replace at a same time not all valued..
+
+    mostly use const and let variable..
+*/
