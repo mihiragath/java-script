c73554e60a504c05a8f916baf2afc30014d415a0
mihiragath <mihiragath8@gmail.com>
2024-11-30 14:50:01 +0530

    new

---
+++ java script basics/comparision.js
@@ -0,0 +1,24 @@
+//these all are basic comparison
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+//mostle this type of comparison are avoid by programmer
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+//if undefined are with any comparison then the value are always false with compare to any type of comparison operator
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === in this case check the daya type are same or not..
+
+console.log("2" === 2);
--- java script basics/conversionOperation.js
+++ java script basics/conversionOperation.js
@@ -1,4 +1,4 @@
-let score = "hitesh"
+let score = "Mihir"
 
 //console.log(typeof score);
 //console.log(typeof(score));
@@ -12,14 +12,14 @@ let valueInNumber = Number(score)
 // "33abc" => NaN
 //  true => 1; false => 0
 
-let isLoggedIn = "hitesh"
+let isLoggedIn = "Mihir"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 //  console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
-// "hitesh" => true
+// "Mihir" => true
 
 let someNumber = 33
 
@@ -41,7 +41,7 @@ let negValue = -value
 // console.log(2%3);
 
 let str1 = "hello"
-let str2 = " hitesh"
+let str2 = " Mihir"
 
 let str3 = str1 + str2
 // console.log(str3);
@@ -51,7 +51,7 @@ let str3 = str1 + str2
 // console.log("1" + 2 + 2);
 // console.log(1 + 2 + "2");
 
-// console.log( (3 + 4) * 5 % 3);
+console.log( (3 + 4) * 5 % 3);
 
 // console.log(+true);
 // console.log(+"");
@@ -64,5 +64,5 @@ let gameCounter = 100
 ++gameCounter;
 console.log(gameCounter);
 
-// link to study
+//link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
